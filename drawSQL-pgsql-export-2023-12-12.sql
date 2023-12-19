73e64bf359e1f3a67085fb13aa039f96698def24
Kiady fanomezantsoa <132125202+Rkotomaalla@users.noreply.github.com>
2023-12-19 13:00:06 +0100

    sql

--- drawSQL-pgsql-export-2023-12-12.sql
+++ drawSQL-pgsql-export-2023-12-12.sql
@@ -1,24 +1,37 @@
-CREATE TABLE "look_matiere"(
-    "id_look_matiere" serial NOT NULL,
-    "id_matiere" INTEGER NOT NULL UNIQUE,
-    "id_look" INTEGER NOT NULL UNIQUE
+CREATE TABLE type (
+    id_type SERIAL PRIMARY KEY,
+    Nom VARCHAR(255) NOT NULL
 );
-ALTER TABLE
-    "look_matiere" ADD PRIMARY KEY("id_look_matiere");
-CREATE TABLE "matiere"(
-    "id_matiere" serial NOT NULL,
-    "nom" VARCHAR(255) NOT NULL,
-    "luxure" INTEGER NOT NULL
+CREATE TABLE style (
+    id_style SERIAL PRIMARY KEY,
+    Nom VARCHAR(255) NOT NULL
 );
-ALTER TABLE
-    "matiere" ADD PRIMARY KEY("id_matiere");
-CREATE TABLE "look"(
-    "id_look" serial NOT NULL,
-    "nom" VARCHAR(255) NOT NULL
+CREATE TABLE matiere(
+    id_matiere SERIAL PRIMARY KEY,
+    Nom VARCHAR(255) NOT NULL
 );
-ALTER TABLE
-    "look" ADD PRIMARY KEY("id_look");
-ALTER TABLE
-    "look_matiere" ADD CONSTRAINT "look_matiere_id_matiere_foreign" FOREIGN KEY("id_matiere") REFERENCES "matiere"("id_matiere");
-ALTER TABLE
-    "look_matiere" ADD CONSTRAINT "look_matiere_id_look_foreign" FOREIGN KEY("id_look") REFERENCES "look"("id_look");
+CREATE  TABLE taille(
+    id_taille SERIAL PRIMARY KEY,
+    Nom VARCHAR(255) NOT NULL,
+    Unite INTEGER NOT NULL
+);
+
+CREATE TABLE poketra(
+    id_poketra SERIAL PRIMARY KEY,
+    Nom VARCHAR(255) NOT NULL,
+    id_style INTEGER NOT NULL,
+    id_type INTEGER NOT NULL,
+    FOREIGN KEY (id_style) REFERENCES style(id_style),
+    FOREIGN KEY (id_type) REFERENCES type(id_type)
+);
+
+CREATE TABLE production(
+    id_production SERIAL PRIMARY KEY,
+    id_matiere INTEGER NOT NULL,
+    id_taille INTEGER NOT NULL,
+    id_poketra INTEGER NOT NULL,
+    quatite DECIMAL(10,2) NOT NULL,
+    FOREIGN KEY (id_matiere) REFERENCES matiere(id_matiere),
+    FOREIGN KEY (id_taille) REFERENCES taille(id_taille),
+    FOREIGN KEY (id_poketra) REFERENCES poketra(id_poketra)
+);
